293df00ed25dfe9555bac96eae5e0f94e38381f7
Matt Garvin <MattGarvin@Matts-MacBook-Air.local>
2016-04-18 19:34:41 -0400

    one more change to error messages

--- public/project/client/views/register/register.controller.js
+++ public/project/client/views/register/register.controller.js
@@ -13,47 +13,42 @@
         init();
 
         function register(user) {
-            if (!user) {
-                $rootScope.errorMessage = "You haven't entered any information."
+            if (!user || !user.email) {
+                $rootScope.errorMessage = "Email address is invalid.";
             }
             else {
-                if (!user.email) {
-                    $rootScope.errorMessage = "Email address is invalid.";
-                }
-                else {
-                    if (user.password == user.password2 || !user.password || !user.password2) {
-                        if (user.firstName && user.lastName) {
-                            newUser.roles = ['standard'];
-                            newUser.email = user.email;
-                            newUser.password = user.password;
-                            newUser.firstName = user.firstName;
-                            newUser.lastName = user.lastName;
-                            UserService
-                                .register(newUser)
-                                .then(
-                                    function (response) {
-                                        var user = response.data;
-                                        if (user != null) {
-                                            $rootScope.currentUser = user;
-                                            $location.url("/home");
-                                        }
-                                        else {
-                                            $rootScope.errorMessage = "A user with that email address already exists."
-                                        }
-
-                                    },
-                                    function (err) {
-                                        vm.error = err;
+                if (user.password == user.password2 || !user.password || !user.password2) {
+                    if (user.firstName && user.lastName) {
+                        newUser.roles = ['standard'];
+                        newUser.email = user.email;
+                        newUser.password = user.password;
+                        newUser.firstName = user.firstName;
+                        newUser.lastName = user.lastName;
+                        UserService
+                            .register(newUser)
+                            .then(
+                                function (response) {
+                                    var user = response.data;
+                                    if (user != null) {
+                                        $rootScope.currentUser = user;
+                                        $location.url("/home");
                                     }
-                                );
-                        }
-                        else {
-                            $rootScope.errorMessage = "You must enter a first and last name.";
-                        }
+                                    else {
+                                        $rootScope.errorMessage = "A user with that email address already exists."
+                                    }
+
+                                },
+                                function (err) {
+                                    vm.error = err;
+                                }
+                            );
                     }
                     else {
-                        $rootScope.errorMessage = "Passwords do not match.";
+                        $rootScope.errorMessage = "You must enter a first and last name.";
                     }
+                }
+                else {
+                    $rootScope.errorMessage = "Passwords do not match.";
                 }
             }
 
