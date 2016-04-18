c2a8dd1bd095ac272fede654879afbf9cb4cfb9f
Matt Garvin <MattGarvin@Matts-MacBook-Air.local>
2016-04-18 19:33:23 -0400

    tweaked error messages

--- public/project/client/views/register/register.controller.js
+++ public/project/client/views/register/register.controller.js
@@ -13,42 +13,47 @@
         init();
 
         function register(user) {
-            if (!user.email) {
-                $rootScope.errorMessage = "Email address is invalid.";
+            if (!user) {
+                $rootScope.errorMessage = "You haven't entered any information."
             }
-            else{
-                if (user.password == user.password2 || !user.password || !user.password2) {
-                    if (user.firstName && user.lastName) {
-                        newUser.roles = ['standard'];
-                        newUser.email = user.email;
-                        newUser.password = user.password;
-                        newUser.firstName = user.firstName;
-                        newUser.lastName = user.lastName;
-                        UserService
-                            .register(newUser)
-                            .then(
-                                function (response) {
-                                    var user = response.data;
-                                    if (user != null) {
-                                        $rootScope.currentUser = user;
-                                        $location.url("/home");
-                                    }
-                                    else {
-                                        $rootScope.errorMessage = "A user with that email address already exists."
-                                    }
-
-                                },
-                                function (err) {
-                                    vm.error = err;
-                                }
-                            );
-                    }
-                    else {
-                        $rootScope.errorMessage = "You must enter a first and last name.";
-                    }
+            else {
+                if (!user.email) {
+                    $rootScope.errorMessage = "Email address is invalid.";
                 }
                 else {
-                    $rootScope.errorMessage = "Passwords do not match.";
+                    if (user.password == user.password2 || !user.password || !user.password2) {
+                        if (user.firstName && user.lastName) {
+                            newUser.roles = ['standard'];
+                            newUser.email = user.email;
+                            newUser.password = user.password;
+                            newUser.firstName = user.firstName;
+                            newUser.lastName = user.lastName;
+                            UserService
+                                .register(newUser)
+                                .then(
+                                    function (response) {
+                                        var user = response.data;
+                                        if (user != null) {
+                                            $rootScope.currentUser = user;
+                                            $location.url("/home");
+                                        }
+                                        else {
+                                            $rootScope.errorMessage = "A user with that email address already exists."
+                                        }
+
+                                    },
+                                    function (err) {
+                                        vm.error = err;
+                                    }
+                                );
+                        }
+                        else {
+                            $rootScope.errorMessage = "You must enter a first and last name.";
+                        }
+                    }
+                    else {
+                        $rootScope.errorMessage = "Passwords do not match.";
+                    }
                 }
             }
 
